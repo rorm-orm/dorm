a06f5d10dc0ca5cb0c98ac53b36462c808ae3269
WebFreak001 <gh@webfreak.org>
2022-11-23 00:07:24 +0100

    Improve and fix dependency downloader

--- download_dependencies.d
+++ download_dependencies.d
@@ -1,26 +1,39 @@
 import fs = std.file;
 import std.algorithm;
 import std.array;
+import std.conv;
+import std.path;
 import std.process;
 import std.stdio;
 import std.string;
 
+debug = Verbose;
+
+__gshared bool color;
+
 int main(string[] args)
 {
+	version (Posix)
+		color = true;
+
+	if (environment.get("NO_COLOR").length)
+		color = false;
+
 	auto archIndex = args.countUntil("ARCH");
 	auto platformIndex = args.countUntil("PLATFORM");
 
 	if (archIndex == -1 || platformIndex == -1)
 	{
-		stderr.writeln("Usage: ", args[0], " ARCH [archs...] PLATFORM [platforms...]");
+		errorln("Usage: ", args[0], " ARCH [archs...] PLATFORM [platforms...]");
 		return 1;
 	}
 
 	auto platforms = determinePlatforms(args[archIndex + 1 .. platformIndex], args[platformIndex + 1 .. $]);
+	auto nativePlatform = TargetPlatform.currentPlatform;
 
 	if (!platforms.mapNames.length)
 	{
-		stderr.writeln("The target platform is not yet supported by DORM, ",
+		errorln("The target platform is not yet supported by DORM, ",
 			"please open an issue or pull request for the target binary name! (DUB arguments: ",
 			args[1 .. $], ")");
 
@@ -30,25 +43,40 @@ int main(string[] args)
 	if (fs.exists(platforms.libname))
 		return 0; // already downloaded & validated or manually compiled
 
+	logln("Missing ", platforms.libname.bold, " in dependency ", `"dorm"`.bold, ". Downloading pre-compiled binary from GitHub...");
+	logln("File signatures are not going to be checked, they have however been confirmed to match with the checksums included in dorm.");
+
 	try
 	{
-		downloadTool("rorm-cli", platforms.cliname, platforms, false, "`dub run dorm` will not work properly - download rorm-cli manually!");
+		if (!fs.exists(nativePlatform.cliname))
+		{
+			bool hasCLI = downloadTool("rorm-cli", nativePlatform.cliname, nativePlatform, false, "`dub run dorm` will not work properly - download rorm-cli manually!");
+
+			if (!hasCLI)
+				logln("Warning: found no pre-compiled binary for rorm-cli. Running `dub run dorm` will not invoke the CLI.");
+		}
+
 		downloadTool("rorm-lib", platforms.libname, platforms, true, "Please manually compile rorm-lib for this platform.");
+
+		logln("Done! DORM will reuse the rorm-lib binary from now on. Downloaded to: ", buildPath(fs.getcwd, platforms.libname).bold);
 
 		return 0;
 	}
 	catch (Exception e)
 	{
-		stderr.writeln("Failed to download precompiled rorm binaries: ", e.msg);
-		stderr.writeln();
-		stderr.writeln("TIP: you can manually compile rorm-lib to skip the automatic download step");
-		stderr.writeln("Expected to find or download '", platforms.libname, "' in '", fs.getcwd, "'");
-		stderr.writeln("DUB target: ", args[1 .. $]);
+		debug (Verbose)
+			errorln("Failed to download precompiled rorm binaries: ", e.toString);
+		else
+			errorln("Failed to download precompiled rorm binaries: ", e.msg);
+		logln();
+		logln("TIP: you can manually compile rorm-lib to skip the automatic download step");
+		logln("Expected to find or download '", platforms.libname, "' in '", fs.getcwd, "'");
+		logln("DUB target: ", args[1 .. $]);
 		return 1;
 	}
 }
 
-void downloadTool(string name, string output, TargetPlatform target, bool required, string errorSupplemental = null)
+bool downloadTool(string name, string output, TargetPlatform target, bool required, string errorSupplemental = null)
 {
 	bool inSection = false;
 	MapLine[] foundLines;
@@ -72,9 +100,9 @@ void downloadTool(string name, string output, TargetPlatform target, bool requir
 			throw new Exception("Does not have any precompiled binaries for this platform for " ~ name
 				~ (errorSupplemental.length ? " - " ~ errorSupplemental : ""));
 		else
-			stderr.writeln("Warning: Does not have any precompiled binaries for this platform for " ~ name
+			errorln("Warning: Does not have any precompiled binaries for this platform for " ~ name
 				~ (errorSupplemental.length ? " - " ~ errorSupplemental : ""));
-		return;
+		return false;
 	}
 
 	// first mapName takes priority
@@ -84,12 +112,13 @@ void downloadTool(string name, string output, TargetPlatform target, bool requir
 		{
 			if (found.platformName == wanted)
 			{
-				auto unverifiedOutput = output ~ ".unverified";
+				auto unverifiedOutput = output ~ ".unverified" ~ target.archiveExtension;
 				download(found.url, unverifiedOutput);
 				validateSHA512(unverifiedOutput, found.sha512);
-				fs.rename(unverifiedOutput, output);
-				extractAndDelete(output);
-				return;
+				fs.rename(unverifiedOutput, output ~ target.archiveExtension);
+				extractAndDelete(output ~ target.archiveExtension);
+				markExecutable(output);
+				return true;
 			}
 		}
 	}
@@ -130,6 +159,48 @@ struct TargetPlatform
 	static TargetPlatform windows(string[] mapNames, string[] archs, string[] platforms)
 	{
 		return TargetPlatform(mapNames, archs, platforms, "windows");
+	}
+
+	static TargetPlatform currentPlatform()
+	{
+		string[] archs;
+		string[] platforms;
+
+		version (AArch64)
+			archs ~= "aarch64";
+		version (ARM)
+			archs ~= "arm";
+		version (X86)
+			archs ~= "x86";
+		version (X86_64)
+			archs ~= "x86_64";
+
+		version (Posix)
+			platforms ~= "posix";
+		version (OSX)
+			platforms ~= "osx";
+		version (linux)
+			platforms ~= "linux";
+		version (BSD)
+			platforms ~= "bsd";
+		version (FreeBSD)
+			platforms ~= "freebsd";
+		version (OpenBSD)
+			platforms ~= "openbsd";
+		version (Windows)
+			platforms ~= "windows";
+
+		return determinePlatforms(archs, platforms);
+	}
+
+	string archiveExtension() const @property
+	{
+		if (platforms.canFind("posix"))
+			return ".tar.gz";
+		else if (platforms.canFind("windows"))
+			return ".zip";
+		else
+			throw new Exception("unsupported platform for archiveExtension");
 	}
 
 	string libname() const @property
@@ -181,7 +252,8 @@ TargetPlatform determinePlatforms(string[] archs, string[] platforms)
 
 void download(string url, string file)
 {
-	auto res = spawnProcess(["wget", url, "-O", file]).wait;
+	logln("Downloading DORM dependency from ", url);
+	auto res = spawnProcess(["wget", url, "-q", "--show-progress", "-U", "[download_dependencies] https://github.com/rorm-orm/dorm", "-O", file]).wait;
 	if (res != 0)
 		throw new Exception("Failed to download " ~ url ~ " to " ~ file);
 }
@@ -192,6 +264,8 @@ void validateSHA512(string file, string sha512)
 	import std.digest.sha;
 	import std.string;
 
+	logln("\tValidating integrity...");
+
 	auto got = sha512Of(cast(ubyte[]) fs.read(file));
 
 	if (got.toHexString.toUpper != sha512.toUpper)
@@ -200,16 +274,18 @@ void validateSHA512(string file, string sha512)
 
 void extractAndDelete(string file)
 {
+	logln("\tExtracting archive...");
+
 	if (file.endsWith(".tar.gz"))
 	{
-		if (spawnProcess(["tar", "-xvf", file]).wait != 0)
+		if (spawnProcess(["tar", "-xf", file]).wait != 0)
 			throw new Exception("Failed extracting release binary " ~ file);
 	}
 	else if (file.endsWith(".zip"))
 	{
 		import std.zip;
 
-		auto zip = new ZipArchive(fs.read(file));
+		scope zip = new ZipArchive(fs.read(file));
 
 		foreach (name, am; zip.directory)
 		{
@@ -219,4 +295,37 @@ void extractAndDelete(string file)
 				fs.write(name, am.expandedData);
 		}
 	}
+	fs.remove(file);
+}
+
+void markExecutable(string file)
+{
+	fs.setAttributes(file, fs.getAttributes(file) | octal!700);
+}
+
+private static immutable logPrefix      = " \x1b[35;1mdorm \x1b[0;35mHelper \x1b[m";
+private static immutable errorLogPrefix = " \x1b[31;1mdorm \x1b[0;31mHelper \x1b[m";
+
+string bold(string s)
+{
+	if (color)
+		return "\x1b[1m" ~ s ~ "\x1b[0m";
+	else
+		return s;
+}
+
+void logln(T...)(T args)
+{
+	if (color)
+		writeln(logPrefix, args);
+	else
+		writeln(args);
+}
+
+void errorln(T...)(T args)
+{
+	if (color)
+		stderr.writeln(errorLogPrefix, args);
+	else
+		stderr.writeln(args);
 }
