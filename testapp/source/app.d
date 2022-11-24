50fe6a1647b5a96bdb97ebfd584259e851aa07a5
WebFreak001 <gh@webfreak.org>
2022-11-24 05:00:33 +0100

    improve testapp

--- testapp/source/app.d
+++ testapp/source/app.d
@@ -1,10 +1,6 @@
 import models;
 
-import core.thread;
-import core.time;
-import std.datetime.systime;
-import std.range;
-import std.stdio;
+import faked, std.datetime.systime, std.random, std.stdio;
 
 import dorm.api.db;
 
@@ -15,6 +11,7 @@ void main()
 	@DormPatch!User
 	struct UserSelection
 	{
+		long id;
 		string username;
 		SysTime createdAt;
 	}
@@ -25,25 +22,37 @@ void main()
 	};
 	auto db = DormDB(options);
 
-	foreach (i; 0 .. 10)
+	auto f = new Faker_de(uniform!int);
+
+	foreach (i; 0 .. 2)
 	{
-		auto user = new User();
-		user.username = "Jan " ~ cast(char)('A' + i);
+		@DormPatch!User
+		struct UserInsert
+		{
+			string username;
+			string password;
+			bool isAdmin;
+		}
+
+		UserInsert user;
+		user.username = f.nameName;
 		user.password = "123456";
 		db.insert(user);
-		Thread.sleep(1.seconds);
 	}
 
-	auto oldestJans = db.select!UserSelection
-		.condition(u => Condition.and(
-				u.username.like("Jan%"),
-				u.not.isAdmin
-			))
-		// .order(u => u.createdAt.asc)
-		// .take(5)
+	auto oldestUsers = db.select!UserSelection
+		.condition(u => u.not.isAdmin)
+		.orderBy(u => u.createdAt.asc)
+		.limit(5)
 		.stream();
 
-	writeln("Oldest 5 Jans:");
-	foreach (i, jan; oldestJans)
-		writefln!"#%d %s\tcreated at %s"(i + 1, jan.username, jan.createdAt);
+	writeln("Oldest 5 Users:");
+	foreach (i, user; oldestUsers)
+	{
+		writefln!"#%d %s\tcreated at %s"(i + 1, user.username, user.createdAt);
+
+		// delete first user
+		if (i == 0)
+			db.remove(user);
+	}
 }
