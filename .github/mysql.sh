8b8e7706169e7bfd22994b0483f484a3eb6d56f1
WebFreak001 <gh@webfreak.org>
2022-12-01 11:59:22 +0100

    use localhost in SQL test scripts

--- .github/mysql.sh
+++ .github/mysql.sh
@@ -1,2 +1,2 @@
-mysqladmin -u username -ppassword drop db || true
-mysqladmin -u username -ppassword create db || true
+mysqladmin -h 127.0.0.1 -u username -ppassword drop db || true
+mysqladmin -h 127.0.0.1 -u username -ppassword create db || true
