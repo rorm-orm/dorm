f026c462a187453c4e472aa5519ca9623689d320
WebFreak001 <gh@webfreak.org>
2022-12-01 12:03:15 +0100

    CI improvements

--- .github/mysql.sh
+++ .github/mysql.sh
@@ -1,2 +1,2 @@
-mysqladmin -h 127.0.0.1 -u username -ppassword drop db || true
+mysqladmin -h 127.0.0.1 -u username -ppassword -f drop db || true
 mysqladmin -h 127.0.0.1 -u username -ppassword create db || true
--- .github/postgres.sh
+++ .github/postgres.sh
@@ -1,1 +1,1 @@
-PGPASSWORD=password psql -h 127.0.0.1 -U username db -c 'DROP DATABASE db;' -c 'CREATE DATABASE db;' || true
+PGPASSWORD=password psql -h 127.0.0.1 -U username -c 'DROP DATABASE db;' -c 'CREATE DATABASE db;' || true
